af2da351fd8c305675037b9d4bac7338fce775c1
mtavano <chelotavano.udp@gmail.com>
2022-11-11 18:01:25 -0300

    Add DELETE /nodes endpoint and handler

--- src/internal/api/node_router.go
+++ src/internal/api/node_router.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func routeNodeEndpoints(prefix string, s *Server) {
 	s.server.Post(fmt.Sprintf("%s", prefix), handleFunc(s, postNewNodeHandler))
+	s.server.Delete(fmt.Sprintf("%s", prefix), handleFunc(s, deleteNodeHandler))
 	s.server.Get(fmt.Sprintf("%s/status", prefix), handleFunc(s, getStatusHandler))
 }
