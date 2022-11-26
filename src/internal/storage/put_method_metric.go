e19856b37e88280aafa2e6241b5989761fa2a8c8
mtavano <chelotavano.udp@gmail.com>
2022-11-26 18:45:09 -0300

    Add storage folder

---
+++ src/internal/storage/put_method_metric.go
@@ -0,0 +1,40 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/go-redis/redis/v9"
+	"github.com/pkg/errors"
+)
+
+type PutMethodMetricInput struct {
+	NodeID string
+	Method string
+}
+
+func (s *Store) PutMethodMetric(ctx context.Context, input *PutMethodMetricInput) error {
+	key := fmt.Sprintf("%s-%s", input.NodeID, input.Method)
+
+	result, err := s.db.Get(ctx, key).Result()
+	switch true {
+	case errors.Is(err, redis.Nil):
+		result = "0"
+	case err != nil:
+		return errors.Wrap(err, "storage: Store.PutMethodMetric s.db.Get error")
+	}
+
+	value, err := strconv.Atoi(result)
+	if err != nil {
+		return errors.Wrap(err, "storage: Store.PutMethodMetric strconv.Atoi error")
+	}
+
+	value++
+	err = s.db.Set(ctx, key, value, 0).Err()
+	if err != nil {
+		return errors.Wrap(err, "storage: Store.PutMethodMetric s.db.Set error")
+	}
+
+	return nil
+}
