6a70a928d1fe1aee5292b7a0f59c3166d7da37bf
mtavano <chelotavano.udp@gmail.com>
2022-12-16 00:08:03 -0300

    Update config pkg

--- src/config/config.go
+++ src/config/config.go
@@ -4,10 +4,9 @@ type Config struct {
 	Environment string `envconfig:"environment" required:"true"`
 
 	// node config
-	Chain             string `envconfig:"chain" required:"true"`
-	NodeURL           string `envconfig:"node_url" required:"true"`
-	BlockNumber       string `envconfig:"block_number" default:"1"`
-	BaseChainDataPath string `envconfig:"base_chain_data_path" default:"/data"`
+	BlockNumber      string            `envconfig:"block_number" default:"1"`
+	BasePathDatabase string            `envconfig:"base_path_database" default:"/data"`
+	NetworksURL      map[string]string `envconfig:"networks_url" required:"true"`
 
 	// server config
 	ApiServerHost string `envconfig:"api_server_host" default:"0.0.0.0."`
