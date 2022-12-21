d64926886fd72765b72ef448346d6290b2e7c522
Marcello Tavano <chelotavano.udp@gmail.com>
2022-12-21 19:18:28 -0300

    fix(proxy): enabling proxy to nodes (#8)
    * Update mods
    
    * Update proxy

--- go.mod
+++ go.mod
@@ -3,25 +3,28 @@ module github.com/darchlabs/nodes
 go 1.18
 
 require (
+	github.com/go-redis/redis/v9 v9.0.0-rc.2
 	github.com/gofiber/fiber/v2 v2.40.1
+	github.com/google/uuid v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/lucasepe/codename v0.2.0
+	github.com/pkg/errors v0.9.1
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v9 v9.0.0-rc.1 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/klauspost/compress v1.15.12 // indirect
-	github.com/lucasepe/codename v0.2.0 // indirect
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.41.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 )
--- src/internal/api/proxy_rpc_request.go
+++ src/internal/api/proxy_rpc_request.go
@@ -1,17 +1,14 @@
 package api
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 
 	"github.com/darchlabs/nodes/src/internal/manager"
 	"github.com/darchlabs/nodes/src/internal/storage"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
 	"github.com/pkg/errors"
 )
 
@@ -24,62 +21,34 @@ type proxyRpcHandlerRequest struct {
 	ID      interface{}   `json:"id"`
 }
 
-func proxyRpcHandler(ctx *Context, c *fiber.Ctx) (interface{}, int, error) {
-	// get path params
-	nodeID := c.Params("node_id")
-	log.Println("request forwarded for for node", nodeID)
+func proxyFunc(ctx *Context) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		nodeID := c.Params("node_id")
 
-	cmd, err := ctx.server.nodesManager.Get(nodeID)
-	if errors.Is(err, manager.ErrNodeNotFound) {
-		return nil, fiber.StatusNotFound, errors.Wrap(ErrNotFound, "api: proxyRpcHandler unrecognized node_id")
-	}
-	if err != nil {
-		return nil, fiber.StatusInternalServerError, errors.Wrap(ErrNotFound, "api: proxyRpcHandler ctx.server.nodesManager.Get error")
-	}
-
-	var req proxyRpcHandlerRequest
-	err = c.BodyParser(&req)
-	if err != nil {
-		return nil, fiber.StatusInternalServerError, errors.Wrap(err, "api: proxyRpcHandler c.BodyParser error")
-	}
-
-	body, err := json.Marshal(req)
-	if err != nil {
-		return nil, fiber.StatusInternalServerError, errors.Wrap(err, "api: proxyRpcHandler json.Marshal error")
-	}
-
-	go func() {
-		err := ctx.store.PutMethodMetric(context.Background(), &storage.PutMethodMetricInput{
-			NodeID: nodeID,
-			Method: req.Method,
-		})
-		if err != nil {
-			log.Printf("error inserting metric for [%s-%s]\n", nodeID, req.Method)
+		nodeInstance, err := ctx.server.nodesManager.Get(nodeID)
+		if errors.Is(err, manager.ErrNodeNotFound) {
+			c.SendStatus(fiber.StatusNotFound)
+			return nil
 		}
-	}()
+		if err != nil {
+			c.SendStatus(fiber.StatusInternalServerError)
+			return nil
+		}
+		nodeURL := fmt.Sprintf("http://localhost:%d/", nodeInstance.Config.Port)
 
-	nodeURL := fmt.Sprintf("http://0.0.0.0:%d/", cmd.Config.Port)
-	request, err := http.NewRequest(http.MethodPost, nodeURL, bytes.NewBuffer(body))
+		go saveOnRedis(ctx, c, nodeID)
+
+		return proxy.Do(c, nodeURL)
+	}
+}
+
+func saveOnRedis(ctx *Context, c *fiber.Ctx, nodeID string) {
+	err := ctx.store.PutMethodMetric(context.Background(), &storage.PutMethodMetricInput{
+		NodeID: nodeID,
+		Method: c.Method(),
+	})
 	if err != nil {
-		return nil, fiber.StatusInternalServerError, errors.Wrap(err, "api: proxyRpcHandler http.NewRequest error")
+		log.Printf("error inserting metric for [%s-%s] %s\n", nodeID, c.Method(), err.Error())
+		return
 	}
-
-	headers := c.GetReqHeaders()
-
-	for k, v := range headers {
-		request.Header.Add(k, v)
-	}
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return nil, fiber.StatusInternalServerError, errors.Wrap(err, "api: proxyRpcHandler http.DefaultClient.Do error")
-	}
-
-	defer response.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, fiber.StatusInternalServerError, errors.Wrap(err, "api: proxyRpcHandler ioutil.ReadAll error")
-	}
-
-	return nil, statusAlreadyProxied, c.Send(bodyBytes)
 }
