a3ba0466f237e72b23ffbfdf2fb01f01fa68e655
mtavano <chelotavano.udp@gmail.com>
2023-02-18 20:30:24 -0300

    Move health endpoint

--- internal/api/node_router.go
+++ internal/api/node_router.go
@@ -2,15 +2,9 @@ package api
 
 import (
 	"fmt"
-
-	"github.com/gofiber/fiber/v2"
 )
 
 func routeNodeEndpoints(prefix string, ctx *Context) {
-	ctx.server.server.Get("/api/health", handleFunc(ctx, func(_ *Context, _ *fiber.Ctx) (interface{}, int, error) {
-		return map[string]string{"status": "runnin"}, fiber.StatusOK, nil
-	}))
-
 	ctx.server.server.Post(fmt.Sprintf("%s", prefix), handleFunc(ctx, postNewNodeHandler))
 	ctx.server.server.Post(fmt.Sprintf("%s/actions", prefix), handleFunc(ctx, postActionHandler))
 	ctx.server.server.Delete(fmt.Sprintf("%s", prefix), handleFunc(ctx, deleteNodeHandler))
