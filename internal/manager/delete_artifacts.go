514e27b7abf04bc8852b17a8225876b49da06941
mtavano <chelotavano.udp@gmail.com>
2023-05-29 22:18:27 -0400

    Add delete_artifacts to manager

---
+++ internal/manager/delete_artifacts.go
@@ -0,0 +1,42 @@
+package manager
+
+import (
+	"context"
+	"log"
+
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (m *Manager) DeleteArtifacts(arts *Artifacts) error {
+	ctx := context.Background()
+
+	// Delete Services
+	for _, svc := range arts.Services {
+		err := m.clusterClient.CoreV1().Services("default").Delete(ctx, svc, metav1.DeleteOptions{})
+		if err != nil {
+			return errors.Wrap(err, "manager: Manager.DeleteArtifacts m.clusterClient.CoreV1().Services().Delete error")
+		}
+		log.Printf("[MANAGER] Delete Service artifact <%s>", svc)
+	}
+
+	// Delete Pods
+	for _, pod := range arts.Pods {
+		err := m.clusterClient.CoreV1().Pods("default").Delete(ctx, pod, metav1.DeleteOptions{})
+		if err != nil {
+			return errors.Wrap(err, "manager: Manager.DeleteArtifacts m.clusterClient.CoreV1().Pods().Delete")
+		}
+		log.Printf("[MANAGER] Delete Pod artifact <%s>", pod)
+	}
+
+	// Delete Deployments
+	for _, deployment := range arts.Deployments {
+		err := m.clusterClient.AppsV1().Deployments("default").Delete(ctx, deployment, metav1.DeleteOptions{})
+		if err != nil {
+			return errors.Wrap(err, "manager: Manager.DeleteArtifacts m.clusterClient.AppsV1().Deployments().Delete")
+		}
+		log.Printf("[MANAGER] Delete Deployment artifact <%s>", deployment)
+	}
+
+	return nil
+}
