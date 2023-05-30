4f7a5437a6e678cec7f008e0d27c3c561dbde58d
mtavano <chelotavano.udp@gmail.com>
2023-05-30 03:06:51 -0400

    Update manager

--- internal/manager/delete_node.go
+++ internal/manager/delete_node.go
@@ -2,17 +2,19 @@ package manager
 
 import (
 	"context"
+	"fmt"
 	"log"
 
+	"github.com/darchlabs/nodes/internal/command"
 	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func (m *Manager) DeleteNode(arts *Artifacts) error {
+func (m *Manager) DeleteNode(ni *NodeInstance) error {
 	ctx := context.Background()
 
 	// Delete Services
-	for _, svc := range arts.Services {
+	for _, svc := range ni.Artifacts.Services {
 		err := m.clusterClient.CoreV1().Services("default").Delete(ctx, svc, metav1.DeleteOptions{})
 		if err != nil {
 			return errors.Wrap(err, "manager: Manager.DeleteArtifacts m.clusterClient.CoreV1().Services().Delete error")
@@ -21,7 +23,7 @@ func (m *Manager) DeleteNode(arts *Artifacts) error {
 	}
 
 	// Delete Pods
-	for _, pod := range arts.Pods {
+	for _, pod := range ni.Artifacts.Pods {
 		err := m.clusterClient.CoreV1().Pods("default").Delete(ctx, pod, metav1.DeleteOptions{})
 		if err != nil {
 			return errors.Wrap(err, "manager: Manager.DeleteArtifacts m.clusterClient.CoreV1().Pods().Delete")
@@ -30,13 +32,23 @@ func (m *Manager) DeleteNode(arts *Artifacts) error {
 	}
 
 	// Delete Deployments
-	for _, deployment := range arts.Deployments {
+	for _, deployment := range ni.Artifacts.Deployments {
 		err := m.clusterClient.AppsV1().Deployments("default").Delete(ctx, deployment, metav1.DeleteOptions{})
 		if err != nil {
 			return errors.Wrap(err, "manager: Manager.DeleteArtifacts m.clusterClient.AppsV1().Deployments().Delete")
 		}
 		log.Printf("[MANAGER] Delete Deployment artifact <%s>", deployment)
 	}
+
+	// Delete folders
+	pathDir := fmt.Sprintf("%s%s/%s/%s", m.basePathDB, ni.Config.Network, ni.Config.Environment, ni.Name)
+	rmDir := command.New("rm", "-rf", pathDir)
+
+	err := rmDir.Start()
+	if err != nil {
+		return errors.Wrap(err, "manager: Manager.DeleteNode rmDir.Start error")
+	}
+	log.Printf("[MANAGER] Delete folder <%s>", pathDir)
 
 	return nil
 }
